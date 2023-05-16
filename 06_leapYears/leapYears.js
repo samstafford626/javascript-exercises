008b44a7f3f8513855223b7768fb87dad882781d
Samuel Stafford <69597989+samstafford626@users.noreply.github.com>
2023-05-16 15:29:28 -0500

    complete function to determine if leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return false;
 
+    if (year % 400 === 0) return true;
+    if (year % 4 === 0 && year % 100 !== 0) return true;
+
+    return false;
 };
 
+console.log(leapYears(1985))
 // Do not edit below this line
 module.exports = leapYears;
