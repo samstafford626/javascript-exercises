d3ec0026158e058e85790e9c14e8c6abe1af3603
Samuel Stafford <69597989+samstafford626@users.noreply.github.com>
2023-05-16 16:03:28 -0500

    Create calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (!array[0]) return 'Empty array'
+  let product = array[0];
+  for (let i = 1; i < array.length; i++) {
+    product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base**exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) return 1
+  let result = 1;
+  for (let i =2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
