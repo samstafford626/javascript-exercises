1e23838dd016eb1274c6f3f889933cda7c814b87
Samuel Stafford <69597989+samstafford626@users.noreply.github.com>
2023-05-16 15:15:27 -0500

    Complete function to sumAll values between two numbers *made it more complicated than necessary but learned a lot

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
 const sumAll = function() {
+    // given two numbers(a lower num and higher num), add every number from the lower # to the higher number in one sum
+    let finalSum = 0;
+    let arrayArgs = Array.from(arguments) //turns array from an object to an array
+    
+    // Tests if their are 2 arguments, and if they are not numbers
+    if (arrayArgs.length !== 2) {
+        return "ERROR"
+    } else if (typeof arrayArgs[0] !== typeof 1 || typeof arrayArgs[1] !== typeof 1) {
+        return "ERROR"
+    }
 
+    // Assigns the low and high number
+    let lowerNum = Math.min(...arrayArgs);
+    let higherNum = Math.max(...arrayArgs);
+
+    // Returns error if negative value is input
+    if (lowerNum < 0 || higherNum < 0) {
+        return "ERROR"
+    } 
+    
+    // Sums all of the numbers
+    while (lowerNum <= higherNum) {
+        finalSum += lowerNum;
+        lowerNum++;
+    }
+
+    return finalSum;
 };
+
+console.log(sumAll(10,90))
 
 // Do not edit below this line
 module.exports = sumAll;
