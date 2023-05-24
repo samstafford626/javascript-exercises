a7c6c07337175dd4b71d507074da87ec9ddabbf8
Samuel Stafford <69597989+samstafford626@users.noreply.github.com>
2023-05-24 15:18:54 -0500

    finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const justTitles = array.map(obj => obj.title);
+    return justTitles
 };
 
 // Do not edit below this line
