5ed1f17a0ea46af6e40e87b9f185d8ffb02c9956
Samuel Stafford <69597989+samstafford626@users.noreply.github.com>
2023-05-24 15:48:31 -0500

    Finished, hard

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    console.table(array);
+    let currentYear = new Date().getFullYear();
+
+    // fix objects with no yearOfDeath
+    let fixedArray = array.map(obj => {
+        if (!obj.yearOfDeath) {
+            return ({
+                name: obj.name,
+                yearOfBirth: obj.yearOfBirth,
+                yearOfDeath: currentYear,
+            })
+        } else {
+            return obj
+        }
+    });
+
+    // sort array from oldest to youngest
+    let sortedArray = fixedArray.sort((a,b) => {
+        let ageA = a.yearOfDeath - a.yearOfBirth;
+        let ageB = b.yearOfDeath - b.yearOfBirth;
+        if (ageA > ageB) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })
+
+    return sortedArray[0]
 
 };
 
