f9d70c66f9b44e7abea7ad14017078fedf92ef5a
Samuel Stafford <69597989+samstafford626@users.noreply.github.com>
2023-05-24 14:34:55 -0500

    complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // splits string into array and filters out non letters
+    const wordArray = word.split("").filter(char => /[a-zA-Z]/.test(char));
+    // stores in forward, joins, and makes lowercase
+    let forwardWord = wordArray.join("").toLowerCase();
+    // reverses the wordArray, joins to string, makes lowercase
+    let reversedWord = wordArray.reverse().join("").toLowerCase();
+    
+    // compares and returns bool
+    return reversedWord === forwardWord ? true : false;
 };
 
 // Do not edit below this line
